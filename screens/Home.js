8bb6e4c77237f7c84fd74828f553a5e09522bc5f
Ethan Storm <stormethan10@gmail.com>
2023-07-17 23:38:15 -0700

    adds new logo/background images

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,8 @@
-import { Box, Text, Button, VStack } from 'native-base'
+import { Box, Text, Button, VStack, Image } from 'native-base'
 
 export default function HomeScreen({ navigation }) {
+  const splashImage = '../assets/splash.png'
+
   return (
     <Box style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text position='absolute' top={10} fontSize='xl'>
