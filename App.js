8178d15a54b22e308b50572d923456acdf4758ff
Ethan Storm <stormethan10@gmail.com>
2023-07-19 11:12:30 -0700

    updated latest build with styling

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
-import 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NativeBaseProvider } from 'native-base';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNav from './components/TabNav';
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NativeBaseProvider } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native'
+import TabNav from './components/TabNav'
+import { StatusBar } from 'expo-status-bar'
 
-import { useColorScheme } from 'react-native';
-import { useState, createContext } from 'react';
-import { bgImageDark, bgImageLight } from './utils/images';
+import { useColorScheme } from 'react-native'
+import { useState, createContext } from 'react'
+import { bgImageDark, bgImageLight } from './utils/images'
 
-import * as Haptics from 'expo-haptics';
+import * as Haptics from 'expo-haptics'
 
-export const ThemeContext = createContext();
-export const UserContext = createContext();
+export const ThemeContext = createContext()
+export const UserContext = createContext()
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  const [room, setRoom] = useState('none');
-  const [colorScheme, setColorScheme] = useState(useColorScheme());
+  const [user, setUser] = useState(null)
+  const [room, setRoom] = useState('none')
+  const [colorScheme, setColorScheme] = useState(useColorScheme())
   const [bgImage, setBgImage] = useState(
     useColorScheme() === 'dark' ? bgImageDark : bgImageLight
-  );
+  )
 
   const toggleTheme = () => {
-    setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
-    setBgImage(colorScheme === 'dark' ? bgImageLight : bgImageDark);
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-  };
+    setColorScheme(colorScheme === 'dark' ? 'light' : 'dark')
+    setBgImage(colorScheme === 'dark' ? bgImageLight : bgImageDark)
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+  }
 
   return (
     <SafeAreaProvider>
-      <StatusBar
-        style='light'
-        hidden={true}
-      />
+      <StatusBar style='light' hidden={true} />
       <NavigationContainer>
         <NativeBaseProvider>
           <UserContext.Provider value={{ user, setUser, room, setRoom }}>
@@ -46,5 +43,5 @@ export default function App() {
         </NativeBaseProvider>
       </NavigationContainer>
     </SafeAreaProvider>
-  );
+  )
 }
